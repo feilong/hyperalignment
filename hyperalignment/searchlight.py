bb654b53b6110a9e97eeb55a5c0c2513f2dd76a6
Ma Feilong <mafeilong@gmail.com>
2021-09-21 13:44:48 -0400

    Add script to create searchlight-based template. Allow disabling weighted average for SL HA.

--- hyperalignment/searchlight.py
+++ hyperalignment/searchlight.py
@@ -1,6 +1,8 @@
 import numpy as np
+from joblib import Parallel, delayed
 
 from .procrustes import procrustes
+from .local_template import compute_template
 
 
 def compute_searchlight_weights(sls, dists, radius):
@@ -21,10 +23,28 @@ def compute_searchlight_weights(sls, dists, radius):
     return weights
 
 
-def searchlight_procrustes(X, Y, sls, dists, radius, T0=None, reflection=True, scaling=False):
+def searchlight_procrustes(X, Y, sls, dists, radius, T0=None, reflection=True, scaling=False, weighted=True):
     T = np.zeros((X.shape[1], Y.shape[1])) if T0 is None else T0.copy()
-    weights = compute_searchlight_weights(sls, dists, radius)
-    for sl, w in zip(sls, weights):
-        t = procrustes(X[:, sl], Y[:, sl], reflection=reflection, scaling=scaling)
-        T[np.ix_(sl, sl)] += t * w[np.newaxis]
+    if weighted:
+        weights = compute_searchlight_weights(sls, dists, radius)
+        for sl, w in zip(sls, weights):
+            t = procrustes(X[:, sl], Y[:, sl], reflection=reflection, scaling=scaling)
+            T[np.ix_(sl, sl)] += t * w[np.newaxis]
+    else:
+        for sl in sls:
+            t = procrustes(X[:, sl], Y[:, sl], reflection=reflection, scaling=scaling)
+            T[np.ix_(sl, sl)] += t
     return T
+
+
+def searchlight_template(dss, sls, dists, radius, n_jobs=1, tmpl_kind='pca'):
+    weights = compute_searchlight_weights(sls, dists, radius)
+    with Parallel(n_jobs=n_jobs, batch_size=1, verbose=1) as parallel:
+        local_templates = parallel(
+            delayed(compute_template)(dss, sl=sl, kind=tmpl_kind, max_npc=len(sl), common_topography=True)
+            for sl in sls)
+
+    tmpl = np.zeros_like(dss[0])
+    for local_template, w, sl in zip(local_templates, weights, sls):
+        tmpl[:, sl] += local_template * w[np.newaxis]
+    return tmpl
