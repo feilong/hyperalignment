Fix a bug of integer overflow.

src/hyperalignment/searchlight.py
@@ -12,7 +12,7 @@ def compute_searchlight_weights(sls, dists, radius):
     """
     weights = compute_searchlight_weights(sls, dists, radius)
     """
-    nv = np.concatenate(sls).max() + 1
+    nv = int(np.concatenate(sls).max()) + 1
     weights_sum = np.zeros((nv,))
     for sl, d in zip(sls, dists):
         w = (radius - d) / radius